72a7d7368fce7dd9a01b5c4b056ad356d0e04d96
ssaaurabh <saurabh.singla@intuzion.com>
2013-06-05 13:13:19 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chd17::Application.config.secret_token = '65ebff950d145372d6d76c80710892dc6e4500ba65eb7aab41f6ecd471030bb0e842fac453e2ccfa29e378a7c25a2017d5aa50aaf1be2f292f61fb1992570e8a'
