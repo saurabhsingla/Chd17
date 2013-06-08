31e4ba00d62d3472bcffc8e1c6d920e6c0ede38d
ssaaurabh <saurabh.singla@intuzion.com>
2013-06-08 18:23:24 +0530

    authentication using devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Chd17::Application.routes.draw do
+  devise_for :users
+
+  resources :dashboard
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +52,7 @@ Chd17::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
