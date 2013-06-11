c887241a9b58467845ca3052331d115ac737bee5
ssaaurabh <saurabh.singla@intuzion.com>
2013-06-11 16:07:38 +0530

    profile pic added for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,12 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :username
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username,
+   :profile_pic
   # attr_accessible :title, :body
+
+  has_attached_file :profile_pic, :styles => {:small=> "100X100>", 
+    :thumb => "40X40>"}, :default_url => 'assets/default_profilepic_#{size}.png'
 
   	# Virtual attribute for authenticating by either username or email
 	# This is in addition to a real persisted field like 'username'
@@ -20,7 +24,8 @@ class User < ActiveRecord::Base
 	def self.find_first_by_auth_conditions(warden_conditions)
       conditions = warden_conditions.dup
       if login = conditions.delete(:login)
-        where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+        where(conditions).where(["lower(username) = :value OR lower(email) = :value", 
+          { :value => login.downcase }]).first
       else
         where(conditions).first
       end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130610101402) do
+ActiveRecord::Schema.define(:version => 20130611095931) do
 
   create_table "address_table_info_asses", :force => true do |t|
     t.integer  "TableInfo_id"
@@ -56,20 +56,24 @@ ActiveRecord::Schema.define(:version => 20130610101402) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "",    :null => false
-    t.string   "encrypted_password",     :default => "",    :null => false
+    t.string   "email",                    :default => "",    :null => false
+    t.string   "encrypted_password",       :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",            :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                :null => false
-    t.datetime "updated_at",                                :null => false
-    t.boolean  "admin",                  :default => false
+    t.datetime "created_at",                                  :null => false
+    t.datetime "updated_at",                                  :null => false
+    t.boolean  "admin",                    :default => false
     t.string   "username"
+    t.string   "profile_pic_file_name"
+    t.string   "profile_pic_content_type"
+    t.integer  "profile_pic_file_size"
+    t.datetime "profile_pic_updated_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
